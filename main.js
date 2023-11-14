fad5521610232f46109d8521c7262a63c102da75
Yosua Daniel <150594726+yosdniel@users.noreply.github.com>
2023-11-15 00:31:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,42 +11,22 @@ document.addEventListener("DOMContentLoaded", async function () {
     async function displayLyrics() {
         for (const line of lyrics) {
             for (const char of line) {
-                const charElement = document.createElement("span"); 
+                const charElement = document.createElement("span");
                 charElement.textContent = char;
-                charElement.style.animation = "glow 2s ease-in-out"; 
-                lyricsElement.appendChild(charElement); 
+                charElement.style.animation = "glow 2s ease-in-out";
+                lyricsElement.appendChild(charElement);
 
                 await new Promise((resolve) => setTimeout(resolve, delay));
-
-                
                 charElement.style.animation = "";
             }
 
-            lyricsElement.appendChild(document.createElement("br")); 
+            // Tambahkan garis baru setelah satu baris lirik ditampilkan
+            lyricsElement.appendChild(document.createElement("br"));
 
             await new Promise((resolve) => setTimeout(resolve, delay * 10));
 
+            // Hapus karakter sebelum menambahkan baris berikutnya
             lyricsElement.textContent = "";
-
-            await new Promise((resolve) => setTimeout(resolve, delay * 10));
         }
 
-        // Panggil fungsi setelah selesai menampilkan lirik
-        showFlowers();
-    }
-
-    // Menampilkan lirik
-    await displayLyrics();
-
-    // Fungsi untuk menampilkan elemen bunga
-    function showFlowers() {
-        var content1 = document.getElementById('lirik');
-        var content2 = document.getElementById('bunga');
-        if (content1 && content2) {
-            content1.innerHTML = content2.innerHTML;
-        } else {
-            console.error('Elemen tidak ditemukan');
-        }
-    }
-    setTimeout(showFlowers, 2000); // Menunggu 2000 milidetik (2 detik) sebelum menampilkan elemen bunga
-});
+       
