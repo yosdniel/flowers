afe232abb4fe5eac365ed6a0d879850db375b18d
Yosua Daniel <150594726+yosdniel@users.noreply.github.com>
2023-11-15 00:01:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,24 +11,21 @@ document.addEventListener("DOMContentLoaded", async function () {
     async function displayLyrics() {
         for (const line of lyrics) {
             for (const char of line) {
-                const charElement = document.createElement("span"); 
+                const charElement = document.createElement("span");
                 charElement.textContent = char;
-                charElement.style.animation = "glow 2s ease-in-out"; 
-                lyricsElement.appendChild(charElement); 
+                charElement.style.animation = "glow 2s ease-in-out";
+                lyricsElement.appendChild(charElement);
 
                 await new Promise((resolve) => setTimeout(resolve, delay));
-
-                
                 charElement.style.animation = "";
             }
 
-            lyricsElement.appendChild(document.createElement("br")); 
+            lyricsElement.appendChild(document.createElement("br"));
 
             await new Promise((resolve) => setTimeout(resolve, delay * 10));
 
+            // Hapus karakter dan garis baru sebelum menambahkan baris berikutnya
             lyricsElement.textContent = "";
-
-            await new Promise((resolve) => setTimeout(resolve, delay * 10));
         }
 
         // Panggil fungsi setelah selesai menampilkan lirik
