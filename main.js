f0e6dc3b8b3ddf4f1efcc1ff288ac2a30bae9cae
Yosua Daniel <150594726+yosdniel@users.noreply.github.com>
2023-11-14 22:42:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,24 +9,23 @@ window.addEventListener('DOMContentLoaded', function () {
         "But I just want you to know",
     ];
 
-    const delay = 45; // adjust the speed here
+    const delay = 45;
     const lyricsElement = document.getElementById("lyrics");
 
     async function displayLyrics() {
         for (const line of lyrics) {
             for (const char of line) {
-                const charElement = document.createElement("span"); 
+                const charElement = document.createElement("span");
                 charElement.textContent = char;
-                charElement.style.animation = "glow 2s ease-in-out"; 
-                lyricsElement.appendChild(charElement); 
+                charElement.style.animation = "glow 2s ease-in-out";
+                lyricsElement.appendChild(charElement);
 
                 await new Promise((resolve) => setTimeout(resolve, delay));
 
-                
                 charElement.style.animation = "";
             }
 
-            lyricsElement.appendChild(document.createElement("br")); 
+            lyricsElement.appendChild(document.createElement("br"));
 
             await new Promise((resolve) => setTimeout(resolve, delay * 10));
 
@@ -35,14 +34,16 @@ window.addEventListener('DOMContentLoaded', function () {
             await new Promise((resolve) => setTimeout(resolve, delay * 10));
         }
 
-    setTimeout(function () {
-        var content1 = document.querySelector('.lirik');
-        var content2 = document.querySelector('.flowers');
+        let content1 = document.querySelector('.lirik');
+        let content2 = document.querySelector('.flowers');
 
         if (content1 && content2) {
             content1.innerHTML = content2.innerHTML;
         } else {
             console.error('Elemen tidak ditemukan');
         }
-    }, 1200);
+    }
+
+    // Panggil fungsi untuk menampilkan lirik
+    displayLyrics();
 });
